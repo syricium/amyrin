d4175076d55624de3dae8255541eb9f3940f0392
jotte <71946106+jottew@users.noreply.github.com>
2023-01-23 22:02:37 +0100

    sdf

--- bot/setup.sh
+++ bot/setup.sh
@@ -1,4 +1,7 @@
 SERVICE_FILE=./bot.service
+BASHRC_FILE=~/.bashrc
+BASHRC_LINE="export XDG_RUNTIME_DIR=/run/user/$(id -u)"
+
 if [ ! -f "$SERVICE_FILE" ]; then
     echo "bot.service file does not exist, please create it with \"touch bot.service\" and then" \
     "configure it with an editor like nano or vim"
@@ -14,7 +17,8 @@ python3.11 -m pip install -r requirements.txt
 python3.11 -m playwright install chromium
 python3.11 -m playwright install-deps
 
-sudo mv bot.service /etc/systemd/user/bot.service
+mkdir -p ~/.config/systemd/user
+sudo mv bot.service ~/.config/systemd/user/bot.service
 
 echo "Script is done running, if you have run the web's setup.sh script already, please remove " \
 "sudo privileges from the user running the bot, and if you don't have one, please create one"
--- web/setup.sh
+++ web/setup.sh
@@ -1,4 +1,7 @@
 SERVICE_FILE=./web.service
+BASHRC_FILE=~/.bashrc
+BASHRC_LINE="export XDG_RUNTIME_DIR=/run/user/$(id -u)"
+
 if [ ! -f "$SERVICE_FILE" ]; then
     echo "web.service file does not exist, please create it with \"touch web.service\" and then" \
     "configure it with an editor like nano or vim"
@@ -13,7 +16,8 @@ sudo apt install -y python3.11
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-sudo mv web.service /etc/systemd/user/web.service
+mkdir -p ~/.config/systemd/user
+sudo mv web.service ~/.config/systemd/user/web.service
 
 echo "Script is done running, if you have run the bot's setup.sh script already, please remove " \
 "sudo privileges from the user running the bot, and if you don't have one, please create one"
