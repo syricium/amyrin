9841ddd55e78d0be7fd5bbd47e376824a5c10b6d
syrice <71946106+syricium@users.noreply.github.com>
2023-02-05 02:05:57 +0100

    Update docs.py

--- bot/modules/views/docs.py
+++ bot/modules/views/docs.py
@@ -128,7 +128,7 @@ class DocView(View):
         docs = await self.scraper.get_documentation(name, updater=update)
 
         if docs is None:
-            return await self._send(interaction, "Could not find element.")
+            return
 
         examples_button: discord.ui.Button = discord.utils.get(
             self.children, custom_id="show_ex"
