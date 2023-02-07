5da2ae67814821fe6e98c59694482f1198d99b8d
syrice <jottew@yahoo.com>
2023-02-07 22:13:29 +0100

    thing

--- bot/modules/events/errors.py
+++ bot/modules/events/errors.py
@@ -1,11 +1,13 @@
 # credits to https://gist.github.com/EvieePy/7822af90858ef65012ea500bcecf1612
 
+import random
 import sys
 import traceback
 
 import discord
 from discord import app_commands
 from discord.ext import commands
+import humanfriendly
 
 
 class CommandErrorHandler(commands.Cog):
@@ -31,9 +33,6 @@ class CommandErrorHandler(commands.Cog):
         if isinstance(error, ignored):
             return
 
-        if isinstance(error, commands.DisabledCommand):
-            await ctx.send(f"{ctx.command} has been disabled.")
-
         elif isinstance(error, commands.NoPrivateMessage):
             try:
                 await ctx.author.send(
@@ -46,12 +45,80 @@ class CommandErrorHandler(commands.Cog):
             param = error.param
             await ctx.send(f"Missing parameter `{param.name}`", ephemeral=True)
 
-        elif isinstance(error, commands.BadArgument):
-            if ctx.command.qualified_name == "tag list":
-                await ctx.send("I could not find that member. Please try again.")
-
         elif isinstance(error, commands.CheckFailure):
             return
+        
+        # thanks chatgpt
+        elif isinstance(error, (app_commands.CommandOnCooldown, commands.CommandOnCooldown)):
+            retry_after = humanfriendly.format_timespan(round(error.retry_after, 1))
+            options = [
+                (
+                    "Prithee, good sir or madam, 'tis requested that thou might wait a moment. "
+                    "Thy command dost require a brief interlude to catch its breath. "
+                    "Tarry but a short while, perchance 'twould be but {}, "
+                    "then thou mayest try thy command anew. Be patient, kind soul, and all shall be well."
+                ),
+                (
+                   "Verily, good friend, thou must grant thy command a brief respite. "
+                   "Tis too hasty, and needs a moment to recover. "
+                   "Wait {}, then try thy command once more. Have faith, all shall be well." 
+                ),
+                (
+                    "Hark! Thou must be patient, gentle user. "
+                    "Thy command hath need of a moment's rest. "
+                    "In {}, thou mayest try thy command again. "
+                    "Fear not, all shall be right in the end."
+                ),
+                (
+                    "Hold, brave user! Thy command doth require a moment of peace. "
+                    "Wait {}, and then thou mayest attempt thy command once more. "
+                    "Have no doubt, all shall turn out well in the end."
+                )
+            ]
+            msg = random.choice(options).format(retry_after)
+            return await ctx.reply(msg)
+        
+        # thanks chatgpt
+        elif isinstance(error, commands.MaxConcurrencyReached):
+            commands.BucketType.default
+            if error.per is commands.BucketType.user or error.per is commands.BucketType.member:
+                msg = (
+                    "Alack and alas, good sir or madam! Thy command hath reached the maximum concurrent usage per user. "
+                    "Pray, do hold thy horses and bide a moment, for the previous command must be completed ere thou mayest proceed. "
+                    "Have patience, kind soul, for all shall be well in due time."
+                )
+            elif error.per is commands.BucketType.category:
+                msg = (
+                    "Good morrow, kind sir/madam. 'Tis a lamentable state of affairs, but thou hast reached the maximum concurrent commands for this category."
+                    "Pray thee, wait until the previous command hath finished ere thou proceedeth with thy request. Thy patience shall be rewarded. Fare thee well!"
+                )
+            elif error.per is commands.BucketType.channel:
+                msg = (
+                    "Greetings, milord/milady. Alas, thou hast reached the utmost limit of concurrent commands per channel. "
+                    "Thou must needs bide thy time until the preceding command is done, and then thou mayest make thy request anew. "
+                    "Thy forbearance shall be rewarded. Fare thee well!"
+                )
+            elif error.per is commands.BucketType.role:
+                msg = (
+                    "Hail, gentle sir/madam. Verily, thou hast reached the pinnacle of concurrent commands per thy role. "
+                    "Thou art requested to be patient and wait until the prior command hath come to a close. "
+                    "Thy diligence shall be rewarded. Fare thee well!"
+                )
+            elif error.per is commands.BucketType.guild:
+                msg = (
+                    "Salutations, noble sir/madam. 'Tis a sorry state, but thou hast reached the zenith of concurrent commands within this guild. "
+                    "Thou must needs tarry until the prior command is completed, and then thou mayest make thy request once more. "
+                    "Thy steadfastness shall be rewarded. Fare thee well!"
+                )
+            else:
+                msg = (
+                    "Good day, esteemed sir/madam. Peradventure, thou hast reached the acme of concurrent commands. "
+                    "Thou must exercise patience and wait until the former command is done. "
+                    "Then and only then, thou may proceed with thy request. Thy perseverance shall be rewarded. Fare thee well!"
+                )
+                
+            return await ctx.reply(msg)
+                
 
         else:
             if await self.bot.is_owner(ctx.author):
